72a3eb20704aa61ff8ba3844e0d882f8303f8312
khagj <a.muraliece2017@gmail.com>
2017-06-22 05:07:13 -0700

    Update and rename alphat.java to hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class hello
+{
+    public static void main(String args[])
+    {
+    	int n = 4;
+    	String f="Hello";
+    	for(int i=0;i<=n;i++)
+    	{
+    		System.out.println(f);
+        }
+    }
+}
